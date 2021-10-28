0905abfe1e41c0e0e5e5326fd371f195a5b4d41c
David <davidroussel171113@outlook.fr>
2021-10-28 20:31:56 +0200

    Ajout des composent

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import signLog from "@/components/signLog";
+import userLogin from "@/components/userLogin";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'signLog',
+    component: signLog
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/id',
+    name: 'userLogin',
+    component: userLogin
   }
 ]
 
