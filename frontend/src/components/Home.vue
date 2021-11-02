7dca562a3705e8924bbd5ce357dcaffdc01a01ec
David <davidroussel171113@outlook.fr>
2021-11-02 22:34:18 +0100

    Mise en place d'un composent acceuil

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="conteneur">
+    <Nav/>
+    <h2>Bienvenue sur le reseau Groupomania</h2>
+  </div>
+</template>
+
+<script>
+import Nav from "../components/Nav"
+export default {
+components : {
+  'Nav' : Nav,
+}
+}
+</script>
