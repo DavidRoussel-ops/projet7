79fa209194f2b80a631f1d79240ecd69830aa176
David <davidroussel171113@outlook.fr>
2021-11-02 22:32:20 +0100

    Mise en place d'un composent qui permet l'enregistrement utilisateur

---
+++ frontend/src/components/SignUp.vue
@@ -0,0 +1,46 @@
+<template>
+  <form>
+    <input v-model="dataSign.mail" placeholder="e-mail"><br>
+    <input v-model="dataSign.pass" placeholder="mots de passe"><br>
+    <input v-model="dataSign.lname" placeholder="Nom"><br>
+    <input v-model="dataSign.fname" placeholder="Prénom"><br>
+    <button @click="sendSign()">Inscription</button>
+    <p v-if="goodSign">{{ message }}</p>
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data(){
+    return{
+      dataSign : {
+        mail : '',
+        pass : '',
+        lname: '',
+        fname: '',
+      },
+      dataSignGood : '',
+      goodSign : false,
+      message : '',
+    }
+  },
+  methods : {
+    sendSign() {
+      this.dataSignGood = JSON.stringify(this.dataSign)
+      axios.post('http://localhost:3000/users/create', this.dataSignGood, {headers : {'Content-Type' : 'application/json'}})
+      .then(response => {
+        let sign = JSON.parse(response.data);
+        this.goodSign = true;
+        this.message = sign.message;
+      })
+      .catch(error => {
+        console.log(error);
+        this.goodSign = true;
+        this.message = error;
+      })
+    }
+  }
+}
+</script>
