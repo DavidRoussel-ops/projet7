07cbe34498ba35e29dd1f46bf5b2053268e4e862
David <davidroussel171113@outlook.fr>
2021-11-02 22:31:04 +0100

    Mise en place d'une navigation log/sign

---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="conteneurNav">
+    <button class="btn-nav" @click="sign" >S'inscrire</button>
+    <button class="btn-nav" @click="log">Se connecter</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data(){
+    return{
+    }
+  },
+  methods : {
+    sign : function () {
+      this.$router.push('/sign')
+      },
+    log : function () {
+      this.$router.push('/log')
+    }
+    }
+}
+</script>
+
+<style>
+#conteneurNav{
+  display: flex;
+  flex-direction: row-reverse;
+}
+
+.btn-nav{
+  width: 150px;
+  height: 50px;
+  border-radius: 20px;
+  color: white;
+  background-color: blue;
+}
+</style>
+
