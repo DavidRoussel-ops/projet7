3bcfff995f604e66240bb7300e32ad8e8334bf6a
David <davidroussel171113@outlook.fr>
2021-11-04 19:41:10 +0100

    Mise en place des posts utilisateurs et ajout dun nouveau composent et d'un boutton déconnexion

--- frontend/src/components/Groupomania.vue
+++ frontend/src/components/Groupomania.vue
@@ -1,30 +1,56 @@
 <template>
-  <p>Bonjour et bienvenue {{ dataGet.fname }} {{ dataGet.lname }}</p>
+    <div class="conteneur">
+      <button @click="logout">Déconnexion</button>
+      <div v-for="posts in allPosts" v-bind:key="posts">
+        <p>{{ posts.com }}</p>
+        <p>Un post de {{ posts.lname }} {{ posts.fname }}</p>
+      </div>
+      <newpost/>
+    </div>
 </template>
 
 <script>
 import axios from "axios";
+import newpost from '../components/NewPost'
 
 export default {
+  components : {
+    newpost : newpost
+  },
   data() {
     return {
-      dataGet: {
+      comment : '',
+      userId : '',
+      dataGetUsers: {
         mail : '',
         lname : '',
         fname : '',
-      }
+      },
+      allPosts : [],
+    }
+  },
+  methods : {
+    logout : function () {
+      localStorage.clear();
+      this.$router.push('/')
     }
   },
   mounted() {
+    this.userId = localStorage.userId;
     axios.get('http://localhost:3000/users/', {headers : {Authorization : 'Bearer ' + localStorage.token}})
     .then(response => {
       let user = JSON.parse(response.data);
-      this.dataGet.mail = user[0].mail;
-      this.dataGet.lname = user[0].lname;
-      this.dataGet.fname = user[0].fname;
+      this.dataGetUsers.mail = user[0].mail;
+      this.dataGetUsers.lname = user[0].lname;
+      this.dataGetUsers.fname = user[0].fname;
     })
     .catch(error => {
       console.log(error);
+    })
+    axios.get('http://localhost:3000/posts/', {headers : {Authorization : 'Bearer ' + localStorage.token}})
+    .then(response => {
+      let posted = JSON.parse(response.data);
+      this.allPosts = posted;
     })
   }
 }
