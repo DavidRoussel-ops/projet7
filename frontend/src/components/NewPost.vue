0d2e37375ac990ea763ce3dd49dc1c46d90835b9
David <davidroussel171113@outlook.fr>
2021-11-04 19:42:07 +0100

    Mise en place d'un composent formulaire création de post

---
+++ frontend/src/components/NewPost.vue
@@ -0,0 +1,38 @@
+<template>
+  <form>
+    <input v-model="dataNewPost.com" placeholder="commentaire" type="text"><br>
+    <input v-model="dataNewPost.img" type="button" placeholder="Add Images"><br>
+    <button @click="sendPost()">Posté</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data(){
+    return{
+      dataNewPost : {
+        com: '',
+        img : '',
+        userId : localStorage.userId
+      },
+      dataPostSend : '',
+    }
+  },
+  methods : {
+    sendPost() {
+      this.dataPostSend = JSON.stringify(this.dataNewPost)
+      axios.post('http://localhost:3000/posts/', this.dataPostSend , {headers : {'Content-Type' : 'application/json', Authorization : 'Bearer ' + localStorage.token}})
+      .then(response => {
+        let dataPost = JSON.parse(response.data);
+        console.log(dataPost)
+        window.location.reload();
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
