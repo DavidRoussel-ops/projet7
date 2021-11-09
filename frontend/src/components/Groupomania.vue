ae50e705b0e3b8e08d8ed0ac56bdd99566afca8d
David <davidroussel171113@outlook.fr>
2021-11-09 23:46:37 +0100

    Modification page d'acceuil

--- frontend/src/components/Groupomania.vue
+++ frontend/src/components/Groupomania.vue
@@ -1,42 +1,52 @@
 <template>
     <div class="conteneur">
+      <navhome/>
       <button @click="logout">Déconnexion</button>
-      <div class="style-post" v-for="posts in allPosts" v-bind:key="posts">
-        <p>{{ posts.img }}</p>
-        <p>{{ posts.com }}</p>
-        <p>Un post de {{ posts.lname }} {{ posts.fname }}</p>
-        <button @click="delPost(posts.id)">Supprimer</button>
-        <button @click="getComment(posts.id)">Voir les commentaires</button>
-        <div v-if="postId === posts.id">
+      <newpost/>
+      <div class="style-post" v-for="(post, index) in allPosts" v-bind:key="index">
+        <p>{{ post.img }}</p>
+        <p>{{ post.com }}</p>
+        <p>Un post de {{ post.lname }} {{ post.fname }}</p>
+        <form v-if="postId === post.id">
+          <input v-model="dataPutPost.com" placeholder="commentaire"><br>
+          <input v-model="dataPutPost.img" placeholder="image"><br>
+          <button @click="putPost()">Valider</button>
+        </form>
+        <button @click="getComment(post.id)">Voir les commentaires</button><br>
+        <button @click="delPost(post.id)">Supprimer</button>
+        <div v-if="postId === post.id">
           <div v-for="comments in allComments" v-bind:key="comments">
             <p>{{ comments.content }}</p>
             <p>Commenté par {{ comments.lname }} {{ comments.fname }}</p>
             <button @click="delCom(comments.id)">Supprimer</button>
           </div>
         </div>
-        <div v-if="postId === posts.id">
-        <button class="btn-com" @click="addComment(posts.id)">Ajouter un commentaire</button>
+        <div v-if="postId === post.id">
+        <button class="btn-com" @click="addComment(post.id)">Ajouter un commentaire</button>
         <form v-if="addCom">
           <input v-model="dataNewComment.content" type="text">
-          <button @click="sendComment(posts.id)">Envoyer</button>
+          <button @click="sendComment(post.id)">Envoyer</button>
         </form>
         </div>
       </div>
-      <newpost/>
     </div>
 </template>
 
 <script>
 import axios from "axios";
-import newpost from '../components/NewPost';
+import newpost from '@/components/NewPost';
+import navhome from "@/components/NavHome";
 
 export default {
   components : {
     newpost : newpost,
+    navhome : navhome,
   },
   data() {
     return {
+      showme : localStorage.userId,
       addCom : false,
+      formPut : false,
       commentId : '',
       userId : '',
       postId : '',
@@ -50,7 +60,14 @@ export default {
         content : '',
         userId : '',
       },
+      dataPutPost : {
+        id : '',
+        com : '',
+        img : '',
+        userId : '',
+      },
       dataCommentSend : '',
+      dataPostPut : '',
       allPosts : [],
       allComments : [],
     }
@@ -86,6 +103,20 @@ export default {
             console.log(error)
           })
     },
+    putPost() {
+      this.dataPutPost.userId = localStorage.userId;
+      this.dataPostPut = JSON.stringify(this.dataPutPost);
+      axios.put('http://localhost:3000/posts/' + this.dataPutPost.id , this.dataPostPut , {headers : {'Content-Type' : 'application/json', Authorization : 'Bearer ' + localStorage.token}})
+          .then(response => {
+            let putPostData = JSON.parse(response.data);
+            console.log(putPostData);
+            this.allPosts.com = '';
+            this.allPosts.img = '';
+            this.allPosts.userId = '';
+            this.allPosts.id = '';
+            window.location.reload();
+          })
+    },
     delPost(postId) {
       this.postId = postId;
       axios.delete('http://localhost:3000/posts/' + postId, {headers : {Authorization : 'Bearer ' + localStorage.token}})
@@ -107,20 +138,13 @@ export default {
   },
   mounted() {
     this.userId = localStorage.userId;
-    axios.get('http://localhost:3000/users/', {headers : {Authorization : 'Bearer ' + localStorage.token}})
-    .then(response => {
-      let user = JSON.parse(response.data);
-      this.dataGetUsers.mail = user[0].mail;
-      this.dataGetUsers.lname = user[0].lname;
-      this.dataGetUsers.fname = user[0].fname;
-    })
-    .catch(error => {
-      console.log(error);
-    })
     axios.get('http://localhost:3000/posts/', {headers : {Authorization : 'Bearer ' + localStorage.token}})
     .then(response => {
-      let posted = JSON.parse(response.data);
-      this.allPosts = posted;
+      let posts = JSON.parse(response.data);
+      this.allPosts = posts;
+    })
+    .catch(err => {
+      console.log(err)
     })
   }
 }
@@ -129,5 +153,10 @@ export default {
 <style>
 .style-post{
   border: 1px solid black;
+}
+
+.center {
+  position: absolute;
+  left: 50%;
 }
 </style>
