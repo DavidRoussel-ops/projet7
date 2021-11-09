2067d52a986e29e4bcd69828a9c4a39faa5d3c30
David <davidroussel171113@outlook.fr>
2021-11-09 23:47:23 +0100

    Mise en place d'une nav home

---
+++ frontend/src/components/NavHome.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="forum">Acceuil</button>
+    <button @click="profil">Profile</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+    }
+  },
+  methods : {
+    forum : function () {
+      this.$router.push('/groupomania')
+    },
+    profil : function () {
+      this.$router.push('/profil')
+    }
+  }
+}
+</script>
