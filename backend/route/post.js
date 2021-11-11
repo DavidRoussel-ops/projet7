458702744140b7c78e9486c459313aa098697508
David <davidroussel171113@outlook.fr>
2021-11-11 17:34:40 +0100

    Ajout route admin

--- backend/route/post.js
+++ backend/route/post.js
@@ -9,11 +9,14 @@ try {
     router.post('/', multer, postCtrl.createPost);
     router.put('/:id', userToken, multer ,postCtrl.updatePost);
     router.delete('/:id', userToken, postCtrl.deletePost);
+    router.delete('/admin/:id', userToken, postCtrl.deletePostAdmin);
 
     router.get('/:id/comments', postCtrl.comment);
     router.post('/:id/comments', postCtrl.createCom);
     router.put('/comments/:id', userToken, postCtrl.updateCom);
     router.delete('/comments/:id', userToken, postCtrl.deleteCom);
+    router.delete('/admin/comments/:id', userToken, postCtrl.deleteComAdmin);
+
 } catch (error) {
     console.log(error);
 }
